6de89bb9221755120ef57d74d39e54721f2b96a9
widemic <widemic@gmail.com>
2019-03-01 15:05:35 +0200

    app.component changes -add marker on click  (used for user page for defining node location)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,7 @@ export class AppComponent implements OnInit {
 
   @Input() location$: Observable<any>;
 
-    latitude: number = 26.15;
+  latitude: number = 26.15;
   longitude: number = 44.44;
 
   map: any;
@@ -54,59 +54,89 @@ export class AppComponent implements OnInit {
   constructor(private sensordataservice: SensorDataService) { }
   ngOnInit() {
 
-   // this.location$ = Observable.interval(1000).startWith(0).switchMap(() => this.sensordataservice.getLocation());
+    // this.location$ = Observable.interval(1000).startWith(0).switchMap(() => this.sensordataservice.getLocation());
 
-   this.sensordataservice.getLocation().subscribe((data: SensorDataModel[]) => {
-    this.locationdata = data;
-    this.locationdata.map(item => {
-      this.location_array.push(item.lattitude);
-      this.location_array.push(item.longitude);
-      var lat = Number(item.lattitude);
-      var lng = Number(item.longitude);
-      this.markers.push({lat, lng});
-    })
+    this.sensordataservice.getLocation().subscribe((data: SensorDataModel[]) => {
+      this.locationdata = data;
+      this.locationdata.map(item => {
+        this.location_array.push(item.lattitude);
+        this.location_array.push(item.longitude);
+        var lat = Number(item.lattitude);
+        var lng = Number(item.longitude);
+        this.markers.push({ lat, lng });
+      })
 
 
-    const features = [];
-    console.log(this.markers)
-    for (let i = 0; i < this.markers.length; i++) {
-      const m = this.markers[i];
-      const longitude = m.lng;
-      const latitude = m.lat;
-      console.log(m);
+      const features = [];
+      console.log(this.markers)
+      for (let i = 0; i < this.markers.length; i++) {
+        const m = this.markers[i];
+        const longitude = m.lng;
+        const latitude = m.lat;
+        console.log(m);
 
-      const iconFeature = new ol.Feature({
-        geometry: new ol.geom.Point(ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857'))
+        const iconFeature = new ol.Feature({
+          geometry: new ol.geom.Point(ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857')),
+          name: 'test',
+          str: "strada test"
+        });
+
+        const iconStyle = new ol.style.Style({
+          image: new ol.style.Icon(({
+            anchor: [0.5, 1],
+            src: '../assets/dot.png',
+            color: 'orange'
+          }))
+        });
+
+        iconFeature.setStyle(iconStyle);
+        features.push(iconFeature);
+      }
+
+      const vectorSource = new ol.source.Vector({
+        features: features
       });
 
-      const iconStyle = new ol.style.Style({
-        image: new ol.style.Icon(({
-          anchor: [0.5, 1],
-          src: '../assets/dot.png',
-          color: 'orange'
-        }))
+      const markerVectorLayer = new ol.layer.Vector({
+        source: vectorSource,
       });
 
-      iconFeature.setStyle(iconStyle);
-      features.push(iconFeature);
-    }
+      this.map.addLayer(markerVectorLayer);
+      this.map.on('singleclick', function (evt) {
 
-    const vectorSource = new ol.source.Vector({
-      features: features
-    });
+        console.log(evt);
+      })
 
-    const markerVectorLayer = new ol.layer.Vector({
-      source: vectorSource,
-    });
+      this.map.on("click", (e) => {
+        this.map.forEachFeatureAtPixel(e.pixel, (feature, layer) => {
+          let id: number = feature.getId();
+          console.log(id);
+          console.log(layer)
+        })
+      });
+      function addMarker(lon, lat) {
+        console.log('lon:', lon);
+        console.log('lat:', lat);
+      
+        var iconFeatures = [];
+      
+        var iconFeature = new ol.Feature({
+          geometry: new ol.geom.Point(ol.proj.transform([lon, lat], 'EPSG:4326',
+            'EPSG:3857')),
+          name: 'Null Island',
+          population: 4000,
+          rainfall: 500
+        });
+      
+        vectorSource.addFeature(iconFeature);
+      }
+      this.map.on('singleclick',function(event){
+        var lonLat = ol.proj.toLonLat(event.coordinate);
+        addMarker(lonLat[0], lonLat[1]);
+      });
 
-    this.map.addLayer(markerVectorLayer);
-    this.map.on('click', function (evt) {
-      console.log(evt);
     })
-
-
-  })
-  console.log(this.markers);
+    console.log(this.markers);
 
 
 
@@ -123,11 +153,11 @@ export class AppComponent implements OnInit {
     //   })
 
 
-      
-      
+
+
     // })
-   
-    
+
+
     this.map = new ol.Map({
       target: 'map',
       controls: ol.control.defaults({
@@ -150,7 +180,7 @@ export class AppComponent implements OnInit {
     //   { lat: 44.42, lng: 26.1 },
     //   { lat: 44.46, lng: 26.08 }
     // ];
-    
+
   }
 }
 
